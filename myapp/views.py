fa241eb81bff03ec33f5cb811ca75278f969c5ed
madanabhupal <pmbss143@gmail.com>
2026-03-12 09:49:58 +0530

    Index page with static template

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from myapp.models import Product
 
 # Create your views here.
+
+def index(request):
+  product = Product.objects.all()
+  return render(request, 'myapp/index.html',{'product':'product'})
